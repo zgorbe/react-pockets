0c4d0fd64d264e8f0d7a1d8c00fe5a711831b22f
zoltan.gorbe <zoltan.gorbe@intland.com>
2018-11-02 18:08:53 +0100

    Home component and Menu improved

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,30 +1,64 @@
 import React, { Component } from 'react';
 import { pockectsRef } from '../../firebase';
+import { Jumbotron, Table, Badge } from 'reactstrap';
 
 class Home extends Component {
     state = { 
-        pockets: []
+        pockets: [],
+        total: 0,
+        loading: true
     }
     
     componentWillMount() {
         pockectsRef.on('value', snapshot => {
             let array = [],
-                resultObject = snapshot.val();
+                resultObject = snapshot.val(),
+                total = 0;
 
             array = Object.keys(resultObject).map(key => {
+                total += resultObject[key].balance;
                 return resultObject[key];
             });
 
-            this.setState({ pockets: array });
+            this.setState({ 
+                pockets: array,
+                total: total,
+                loading: false
+            });
         });
     }
     render() { 
-        return ( 
-            <div>
-                {
-                    this.state.pockets.map( (pocket, index) => {
-                        return <div key={ index }>{ pocket.name }</div>
-                    })
+        return (
+            <div className="col-12">
+                { this.state.loading && <div className="loading mx-auto col-md-8"></div> }
+                { !this.state.loading &&
+                    <div className="home">
+                        <Jumbotron>
+                            <h3>Your total balance is { this.state.total }</h3>
+                        </Jumbotron>
+                        <Table className="mx-auto col-md-8">
+                            <thead>
+                                <tr>
+                                    <th>Pocket</th>
+                                    <th>Balance</th>
+                                    <th>Last Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                this.state.pockets.map( (pocket, index) => {
+                                    return <tr key={ index }>
+                                        <td>
+                                            { pocket.name } <Badge color="info">{ Object.keys(pocket.actions).length }</Badge>
+                                        </td>
+                                        <td>{ pocket.balance }</td>
+                                        <td>{ new Date(pocket.timestamp).toLocaleDateString() }</td>
+                                    </tr>
+                                })
+                            }
+                            </tbody>
+                        </Table>
+                    </div>
                 }
             </div> 
         );
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,21 +6,42 @@ import {
     NavbarBrand,
     Nav,
     NavItem,
-    NavLink
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem    
 } from 'reactstrap';
 
 class Menu extends Component {
     state = {  }
+    
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
     render() { 
         return (
             <Navbar color="dark" dark expand="md">
-                <NavbarBrand href="/">Home</NavbarBrand>
-                <NavbarToggler onClick={this.toggle} />
-                <Collapse isOpen={this.state.isOpen} navbar>
+                <NavbarBrand href="/">Pockets</NavbarBrand>
+                <NavbarToggler onClick={ this.toggle } />
+                <Collapse isOpen={ this.state.isOpen } navbar>
                     <Nav className="ml-auto" navbar>
                         <NavItem>
-                            <NavLink href="#/actions">Actions</NavLink>
+                            <NavLink href="#/">Home</NavLink>
                         </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Actions
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>Create Action</DropdownItem>
+                                <DropdownItem>Create Movement</DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem href="#/actions">All Actions</DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>                        
                         <NavItem>
                             <NavLink href="#/login">Logout</NavLink>
                         </NavItem>
